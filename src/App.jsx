c90f2b7fe9185c8b374eb0e197c45cbd430f2a2a
Chinh Nguyen <36144798+NguyenHChinh@users.noreply.github.com>
2025-05-17 23:33:27 -0500

    feat: implement word combo display in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
+  const [combo, setCombo] = useState([])
+
+  useEffect(() => {
+    setCombo(['Snow', 'Ball', 'Game', 'Night', 'Club', 'House', 'Plant']);
+  }, [])
 
   return (
-    <>
-      <h1 className='text-blue-400 text-4xl'>WordChain!</h1>
-    </>
+    <div className='app-container flex flex-col items-center justify-center h-screen bg-blue-200'>
+      <h1 className='absolute text-3xl font-bold text-center top-1'>
+        WordChain
+      </h1>
+
+      {combo.map((word, index) => (
+        <div key={index} className='word-container flex items-center justify-center'>
+          <div className='bg-transparent my-4'>
+            <h2 className='text-4xl font-semibold'>{word}</h2>
+          </div>
+        </div>
+      ))}
+    </div>
   )
 }
 
