c8f21ac88bfa74ef56ff7b164bee33c3db972d21
Chinh Nguyen <36144798+NguyenHChinh@users.noreply.github.com>
2025-05-17 23:50:29 -0500

    feat: add input field for previous word in App component

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import './App.css'
 
 function App() {
   const [combo, setCombo] = useState([])
+  const [previousWord, setPreviousWord] = useState('SNOW ..?')
 
   useEffect(() => {
     setCombo(['SNOW', 'BALL', 'GAME', 'NIGHT', 'CLUB', 'HOUSE', 'PLANT']);
   }, [])
+
 
   return (
     <div className='app-container flex flex-col items-center justify-center h-screen bg-blue-200'>
@@ -22,7 +24,15 @@ function App() {
         </div>
       ))}
 
-
+      <div className='input-container flex items-center justify-center mt-12'>
+        <input
+          className='border-2 border-gray-300 rounded-lg p-2 text-center text-2xl w-1/ bg-white 3'
+          type='text'
+          placeholder={previousWord}
+        >
+        
+        </input>
+      </div>
     </div>
   )
 }
